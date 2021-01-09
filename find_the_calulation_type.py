57f9ea56d895e06163e00ce21fcfbe3dc6b7c7a1
the-carpnter <74491340+the-carpnter@users.noreply.github.com>
2021-01-09 17:02:48 +0530

    Create find_the_calulation_type.py

---
+++ find_the_calulation_type.py
@@ -0,0 +1,5 @@
+def calc_type(a, b, res):
+    if a + b == res: return 'addition'
+    if a - b == res: return 'subtraction'
+    if a * b == res: return 'multiplication'
+    if a / b == res: return 'division'
